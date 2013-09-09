5c988510642477beb015a72c4d56ea25e95537ae
Maciej Małecki <me@mmalecki.com>
2013-09-09 23:45:54 +0200

    [fix] Remove `libc` from `free` and `malloc` probes

--- leak.d
+++ leak.d
@@ -1,18 +1,18 @@
 #!/usr/sbin/dtrace -s
 
-pid$target:libc:malloc:entry
+pid$target::malloc:entry
 {
     self->bytes = arg0;
 }
 
-pid$target:libc:malloc:return
+pid$target::malloc:return
 /self->bytes/
 {
     printf("malloc: %d bytes at %lx\n", self->bytes, arg1);
     self->bytes = 0;
 }
 
-pid$target:libc:free:entry
+pid$target::free:entry
 {
     printf("free: at %lx\n", arg0);
 }
